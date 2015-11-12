372f48e0ddd0eee5df219ad04c623d17387c788a
tkhuynh <thequest2311@yahoo.com>
2015-11-11 22:46:44 -0800

    made seed data, set up api

--- server.js
+++ server.js
@@ -11,7 +11,35 @@ app.use(express.static("public"));
 //set up body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+//seed data
 
+var todos = [
+		{
+			_id: 1,
+			task: "Go to the bank",
+			description: "Withdraw some cash for personal use"
+		},
+		{
+			_id: 2,
+			task: "Go the supermarket",
+			description: "Buy some fish and milk"
+		},
+		{
+			_id: 3,
+			task: "Lunch at noon",
+			description: "See Jane at Totomi's restaurant"
+		}
+];
+
+//setup routes
+
+app.get("/", function (req, res) {
+	res.render("index");
+});
+//set up todos api
+app.get("/api/todos", function (req, res) {
+	res.json(todos);
+});
 
 
 
