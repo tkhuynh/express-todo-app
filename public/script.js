879cb7bf408b83d74715d5c65b29b854ff6775aa
tkhuynh <thequest2311@yahoo.com>
2015-11-11 22:53:04 -0800

    client side, get all todos, made new todo form to create new one add to the list

---
+++ public/script.js
@@ -0,0 +1,32 @@
+$(function() {
+	var baseUrl = "/api/todos";
+	var allTodos = [];
+	var $toDoList = $("#todos-list");
+
+	var source = $("#todos-template").html();
+	var template = Handlebars.compile(source);
+
+	var render = function () {
+		$toDoList.empty();
+		var todosHtml = template({todos: allTodos});
+		$toDoList.append(todosHtml);
+	};
+
+	$.get(baseUrl, function (data) {
+		allTodos = data;
+		render();
+	});
+	$("#create-todos").on("submit", function(event) {
+		event.preventDefault();
+		var newTodo = $(this).serialize();
+		console.log(newTodo);
+		if(allTodos.length > 0) {
+			newTodo._id = allTodos[allTodos.length - 1]._id + 1;
+		}
+		document.getElementById("create-todos").reset();
+		$.post(baseUrl, newTodo, function(data) {
+			allTodos.push(data);
+			render();
+		});
+	});
+});
